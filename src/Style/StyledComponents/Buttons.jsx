1ace4591970b60f5b38c9e78a1d9995cc183aa02
khaledgabdelnasser <louda1905@gmail.com>
2024-03-15 00:49:27 +0200

    handle filter by rate and price

--- src/Style/StyledComponents/Buttons.jsx
+++ src/Style/StyledComponents/Buttons.jsx
@@ -8,3 +8,16 @@ export const DarkButton = styled(Button)(({ theme }) => ({
     bgcolor: theme.palette.primary.dark,
 
 }));
+export const TransparentButton = styled(Button)(({ theme }) => ({
+    fontFamily: "Cairo",
+    fontSize: "15px",
+    fontWeight: 500,
+    lineHeight: "28.11px",
+    letterSpacing: "-0.24 px",
+    textDecoration:"none",
+    textTransform:"none",
+    display:"flex",
+    flexDirection:"row",
+    height:"28px",
+    width:"41px"
+  }));
